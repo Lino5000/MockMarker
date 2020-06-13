c3aaa69f3d77ab1fd41a2718b9c92c431bcecb58
Lino5000 <bathy2oo2@gmail.com>
2020-06-13 13:07:28 +1000

    TeacherMain with stubs for subprograms.

--- Teacher/TeacherMain.py
+++ Teacher/TeacherMain.py
@@ -5,7 +5,7 @@ class TeacherMain:
     def __init__(self):
         self.window = tk.Tk()  # Create the window
         self.window.after(0, self.loop)  # Start the custom loop
-        self.window.mainloop()  # Start the library's window loop
+        self.window.mainloop()  # Start the library's window loop - handles inputs and display
 
     def loop(self):
         question = ChooseQuestion(self.window)
@@ -21,19 +21,20 @@ def clearWindow(window):
 def ChooseQuestion(window):
     # Stub
     clearWindow(window)
-    label = tk.Label(window, text="Choosing Question")
-    label.pack()
-    window.update()
+    label = tk.Label(window, text="Choosing Question")  # Create a Label
+    label.pack()  # Add the label the window - just puts it directly underneath the last element
+    window.update()  # Let the window know there's something new for it to deal with
     return input(': ')
 
 
 def DisplayQuestion(question, window):
     # Stub
-    clearWindow(window)
-    label = tk.Label(window, text=question)
-    label.pack()
+    clearWindow(window)  # Create a Label
+    label = tk.Label(window,
+                     text=question)  # Add the label the window - just puts it directly underneath the last element
+    label.pack()  # Let the window know there's something new for it to deal with
     window.update()
-    input()
+    input()  # Wait for user rather than continuing immediately
 
 
 if __name__ == "__main__":
